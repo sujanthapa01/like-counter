581a42237aa05787cd57c944db68765b1a1d2d07
sujanthapa01 <sujanthapa4001@gmail.com>
2024-02-01 17:19:37 +0530

    commit

---
+++ public/script.js
@@ -0,0 +1,47 @@
+      // Import the functions you need from the SDKs you need
+      import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+      import {getDatabase, set ,get, update ,remove ,ref, child} from "https://www.gstatic.com/firebasejs/10.7.2/firebase-database.js";
+      // TODO: Add SDKs for Firebase products that you want to use
+      // https://firebase.google.com/docs/web/setup#available-libraries
+
+      // Your web app's Firebase configuration
+      // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+      const firebaseConfig = {
+        apiKey: "AIzaSyAdmq9JDsWdBdtTC2sIYloCtOPjTbuQXSA",
+        authDomain: "deft-cove-363816.firebaseapp.com",
+        databaseURL: "https://deft-cove-363816-default-rtdb.firebaseio.com",
+        projectId: "deft-cove-363816",
+        storageBucket: "deft-cove-363816.appspot.com",
+        messagingSenderId: "467462296842",
+        appId: "1:467462296842:web:bce6c704f4d70208e896f3",
+        measurementId: "G-L361KEHQ1X",
+      };
+
+      // Initialize Firebase
+      const app = initializeApp(firebaseConfig);
+    //   const analytics = getAnalytics(app);
+
+
+
+
+const db = getDatabase();
+
+var user = document.querySelector('.name-input');
+var likeBtn = document.querySelector('.btn');
+
+
+function submit(){
+set(ref(db,'people/' + user.value),{
+  Name:user.value
+})
+.then(()=>{
+alert('sucess!')
+})
+.catch((error)=>{
+alert(error)
+});
+
+
+}
+likeBtn.addEventListener('click',submit);
+
---
+++ src/server.js
@@ -0,0 +1,26 @@
+// import { initializeApp } from 'firebase/app';
+// import { getDatabase, ref, set, onValue, runTransaction, once } from 'firebase/database';
+
+// var firebaseConfig = {
+//     apiKey: "AIzaSyAdmq9JDsWdBdtTC2sIYloCtOPjTbuQXSA",
+//     authDomain: "deft-cove-363816.firebaseapp.com",
+//     projectId: "deft-cove-363816",
+//     storageBucket: "deft-cove-363816.appspot.com",
+//     messagingSenderId: "467462296842",
+//     appId: "1:467462296842:web:bce6c704f4d70208e896f3"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// // Get a reference to the Firebase Realtime Database
+// const database = getDatabase(app);
+
+// // Function to get the client's IP address
+// function getClientIP(callback) {
+//     fetch('https://api64.ipify.org?format=json')
+//         .then(response => response.json())
+//         .then(data => callback(data.ip))
+//         .catch(error => console.error('Error fetching IP:', error));
+// }
+
