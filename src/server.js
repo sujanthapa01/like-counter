d12f8e11302daa784a5c14a9e686b4b85e4dc25a
sujanthapa01 <sujanthapa4001@gmail.com>
2024-05-01 14:16:56 +0530

    bug fixes

--- src/server.js
+++ src/server.js
@@ -7,16 +7,15 @@
       // Your web app's Firebase configuration
       // For Firebase JS SDK v7.20.0 and later, measurementId is optional
       const firebaseConfig = {
-        apiKey: "AIzaSyAdmq9JDsWdBdtTC2sIYloCtOPjTbuQXSA",
-        authDomain: "deft-cove-363816.firebaseapp.com",
-        databaseURL: "https://deft-cove-363816-default-rtdb.firebaseio.com",
-        projectId: "deft-cove-363816",
-        storageBucket: "deft-cove-363816.appspot.com",
-        messagingSenderId: "467462296842",
-        appId: "1:467462296842:web:bce6c704f4d70208e896f3",
-        measurementId: "G-L361KEHQ1X",
+        apiKey: process.env.REACT_APP_API_KEY,
+        authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+        databaseURL: process.env.REACT_APP_DATABASE_URL,
+        projectId: process.env.REACT_APP_PROJECT_ID,
+        storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+        messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+        appId: process.env.REACT_APP_APP_ID,
+        measurementId: process.env.REACT_APP_MEASUREMENT_ID
       };
-
       // Initialize Firebase
       const app = initializeApp(firebaseConfig);
     //   const analytics = getAnalytics(app);
